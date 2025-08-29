71cf5b93cad05288c1bad1dfe830c987fbf9b840
awbait <awbait.com@gmail.com>
2025-08-29 11:18:13 +0300

    feat: hide premium block if exist sub

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,28 +1,33 @@
-import Status from "@/components/status";
-import Gift from "@/components/gift";
-import Notes from "@/components/notes";
-import Premium from "@/components/premium";
-import Guide from "@/components/guide";
-import Help from "@/components/help";
-import styles from "./styles.module.scss";
-import "@/styles/variables.scss";
-import { usePlatform } from "@/hooks/usePlatform";
+import Gift from '@/components/gift'
+import Guide from '@/components/guide'
+import Help from '@/components/help'
+import Notes from '@/components/notes'
+import Premium from '@/components/premium'
+import Status from '@/components/status'
+import { useAuthContext } from '@/contexts/AuthContext'
+import { usePlatform } from '@/hooks/usePlatform'
+import '@/styles/variables.scss'
+import styles from './styles.module.scss'
 
 const MainPage = () => {
-  const platform = usePlatform();
-  const marginTop = platform === "pc" ? "" : "-35px";
-  const paddingTop = platform === "pc" ? "10px" : "";
+	const platform = usePlatform()
+	const marginTop = platform === 'pc' ? '' : '-35px'
+	const paddingTop = platform === 'pc' ? '10px' : ''
 
-  return (
-    <div className={styles.container} style={{ marginTop, paddingTop }}>
-      <Status />
-      <Gift />
-      <Notes />
-      <Premium />
-      <Guide />
-      <Help />
-    </div>
-  );
-};
+	const { user } = useAuthContext()
+	const subscription = user?.customerSubscription
+	const isPaid = subscription?.createdVia === 'paid'
 
-export default MainPage;
+	return (
+		<div className={styles.container} style={{ marginTop, paddingTop }}>
+			<Status />
+			<Gift />
+			<Notes />
+			{!isPaid && <Premium />}
+			<Guide />
+			<Help />
+		</div>
+	)
+}
+
+export default MainPage
